ade2ddc8bcf8e86ce387d1a4a0507d2bf02b1c71
Alex Cote <amcote@ugls24.ece.uvic.ca>
2019-07-15 11:58:10 -0700

    started structural hookup

--- dh_hw_mult.h
+++ dh_hw_mult.h
@@ -1,15 +1,21 @@
 #include "systemc.h"
 #include "digit.h"
 #include <iostream>
+#include "hardware_structural.h"
 
 #ifndef _DH_HW_MULT_H_
 #define _DH_HW_MULT_H_ 1
 
 
-#define WAIT_STATE 0
-#define EXECUTE_STATE 1
-#define OUTPUT_STATE 2
-#define FINISH_STATE 3
+#define WAIT_STATE      0
+#define EXECUTE_STATE   1
+#define ES_MULT_LOAD    2
+#define ES_DEASSERT_1   3
+#define ES_PROCESS_IF_1 4
+#define ES_DEASSERT_2   5
+#define ES_PROCESS_IF_2 6
+#define OUTPUT_STATE    7
+#define FINISH_STATE    8
 
 SC_MODULE (dh_hw_mult)
 {
@@ -23,88 +29,27 @@ SC_MODULE (dh_hw_mult)
 	
 	sc_out<bool> hw_mult_done;
 	
-	UINT2 state;
-
-	//void process_hw_mult();
-  
-	void process_hw_mult()
-	{
-	    cout << "process_hw_mult: Entered" << endl;
-	    NN_DIGIT a[2], b, c, t, u;
-	    NN_HALF_DIGIT bHigh, bLow, cHigh, cLow;
-		
-	    state = WAIT_STATE;
-	    hw_mult_done.write(0);
-	    
-		while(true){
-			switch(state)
-			{
-				case WAIT_STATE:
-					//cout << "process_hw_mult: WAIT_STATE" << endl;
-					if(hw_mult_enable.read() == true)
-					{
-						state = EXECUTE_STATE;
-					}
-					//wait(); // Figure out where to put this....
-					break;
-					
-					
-				case EXECUTE_STATE:
-				        //cout << "process_hw_mult: EXECUTE_STATE" << endl;
-					// Read inputs	
-					b = in_data_1.read();
-					c = in_data_2.read();
-					
-					// Original code from NN_DigitMult()...		
-					bHigh = (NN_HALF_DIGIT)HIGH_HALF (b);
-					bLow = (NN_HALF_DIGIT)LOW_HALF (b);
-					cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
-					cLow = (NN_HALF_DIGIT)LOW_HALF (c);
-
-					a[0] = (NN_DIGIT)bLow * (NN_DIGIT)cLow;
-					t = (NN_DIGIT)bLow * (NN_DIGIT)cHigh;
-					u = (NN_DIGIT)bHigh * (NN_DIGIT)cLow;
-					a[1] = (NN_DIGIT)bHigh * (NN_DIGIT)cHigh;
-				  
-					if ((t += u) < u) a[1] += TO_HIGH_HALF (1);
-					u = TO_HIGH_HALF (t);
-				  
-					if ((a[0] += u) < u) a[1]++;
-					a[1] += HIGH_HALF (t);
-					
-					state = OUTPUT_STATE;
-					break;
-					
-					
-				case OUTPUT_STATE:
-				        //cout << "process_hw_mult: OUTPUT_STATE" << endl;
-					// Write outputs
-					out_data_low.write(a[0]);
-					out_data_high.write(a[1]);
-					
-					hw_mult_done.write(true); // assert multiplication is done
-					state = FINISH_STATE;
-					break;
-					
-					
-				case FINISH_STATE:
-				        //cout << "process_hw_mult: FINISH_STATE" << endl;
-					if(hw_mult_enable.read() == false)
-					{
-						hw_mult_done.write(false);
-						state = WAIT_STATE;
-						
-					}
-					//wait();
-					break;
-				
-			
-			} // end switch
-			wait();
-		}// end while true
-		
-			  
-	}
+	UINT4 state;
+	
+	void process_hw_mult();
+	
+	// STRUCTURAL INTERCONNECT OF EXECUTE STATE MULTIPLIER
+	
+	//Registers
+	async_reg b_register, c_register, t_register, u_register, a0_register, a1_register, a1_out_register;
+	
+	
+	// Register Clocks
+	b_register.clock(hw_clock);
+	c_register.clock(hw_clock);
+	t_register.clock(hw_clock);
+	u_register.clock(hw_clock);
+	a0_register.clock(hw_clock);
+	a1_register.clock(hw_clock);
+	a1_out_register.clock(hw_clock);
+	
+	
+	
   
 	SC_CTOR (dh_hw_mult)
 	{
